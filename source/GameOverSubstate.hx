4b8433bc6a19cce2de0b0aaf2c5b035b9549af26
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2022-07-11 14:46:41 -0300

    a

--- source/GameOverSubstate.hx
+++ source/GameOverSubstate.hx
@@ -116,6 +116,11 @@ class GameOverSubstate extends MusicBeatSubstate
 			default:
 				bf.playAnim('firstDeath');
 		}
+		
+		#if android
+		addVirtualPad(NONE, A_B);
+		addPadCamera();
+		#end
 	}
 
 	override function update(elapsed:Float)
