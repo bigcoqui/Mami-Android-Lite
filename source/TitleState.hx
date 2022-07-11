76ae3d11ba0ab23e970f2f36a709ccb638763734
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2022-07-11 13:40:47 -0300

    uma coisa que eu n sei pra que serve

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -51,11 +51,14 @@ class TitleState extends MusicBeatState
 
 	override public function create():Void
 	{
+	  #if android
+	  FlxG.android.preventDefaultKeys = [BACK];
+	  #end
 		
-		#if sys
+		/*#if sys
 		if (!sys.FileSystem.exists(Sys.getCwd() + "/assets/replays"))
 			sys.FileSystem.createDirectory(Sys.getCwd() + "/assets/replays");
-		#end
+		#end*/
 
 		@:privateAccess
 		{
