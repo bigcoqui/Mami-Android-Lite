7a49ca366a4ff0054b3f46430451515533ff758c
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2022-07-25 16:19:01 -0300

    update VideoState.hx

--- source/VideoState.hx
+++ source/VideoState.hx
@@ -77,7 +77,7 @@ class VideoState extends MusicBeatState
 			}
 		}
 
-		GlobalVideo.get().source(leSource);
+		GlobalVideo.get().source(Asset2File.getPath(leSource, ".webm"));;
 		GlobalVideo.get().clearPause();
 		if (GlobalVideo.isWebm)
 		{
@@ -90,7 +90,11 @@ class VideoState extends MusicBeatState
 		} else {
 			GlobalVideo.get().play();
 		}
-		
+
+		// maybe
+		GlobalVideo.getWebm().webm.width = 1280;
+		GlobalVideo.getWebm().webm.height = 720;
+
 		/*if (useSound)
 		{*/
 			//vidSound = FlxG.sound.play(leSource.replace(".webm", ".ogg"));
